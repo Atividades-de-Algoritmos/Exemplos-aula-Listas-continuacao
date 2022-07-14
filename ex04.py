5c9112dd846e5041861d2d4e621d66a2ae8b520c
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-13 21:56:55 -0300

    Maior e menor ⬆⬇

--- ex04.py
+++ ex04.py
@@ -4,22 +4,22 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 28/06/2022
+# data: 13/07/2022
 #
 # 4 – ler uma lista de 5 números e imprimir o menor e maior valor.
-#
-# criar uma lista vazia e adicionar 5 números inteiros a ela.
-lista = []
-# entrada: 5 números inteiros
-for i in range(5): # for para adicionar 5 números inteiros a lista criada anteriormente
-    num = int(input(f"informe um número[{i+1}]: ")) # entrada de número inteiro para adicionar a lista
-    lista.append(num) # adicionar número inteiro a lista criada anteriormente
 
-# saida:
-print("lista:") # imprimir a lista
-print(f"{lista}") # imprimir a lista
-print("menor valor:") # imprimir o menor valor
-print(f"{min(lista)}") # imprimir o menor valor da lista
-print("maior valor:") # imprimir o maior valor
-print(f"{max(lista)}") # imprimir o maior valor da lista
-print("fim do programa") # imprimir fim do programa
+# -- Processamento e entrada de dados --
+
+lista = [] # Criando uma lista vazia e adicionar 5 números inteiros a ela.
+
+for i in range(5): # for para adicionar 5 números inteiros a lista
+    num = int(input(f"Informe um número[{i+1}]: ")) # Solicitando um valor int
+    lista.append(num) # Adicionando número inteiro a lista criada anteriormente
+
+# -- Saída de dados --
+
+print("\nLista:", lista) # Imprimndo a lista
+print("\nMenor valor:", min(lista)) # Imprimindo o menor valor
+print("\nMaior valor:", max(lista)) # imprimindo o maior valor
+
+print("\nfim do programa") # Informando ao usuário que o programa terminou
