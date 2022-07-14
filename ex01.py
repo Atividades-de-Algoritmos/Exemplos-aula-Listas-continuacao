9f785ca9d43eb8963eab7ca7736dca7cee49baf1
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-13 21:55:41 -0300

    Descobrindo Posições 📋

--- ex01.py
+++ ex01.py
@@ -4,24 +4,33 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 28/06/2022
+# data: 13/07/2022
 #
 # 1 – ler uma lista de 5 número inteiros e imprimir cada número juntamente com a sua posição na lista.
-#
-# criar uma lista vazia e adicionar 5 números inteiros a ela.
-lista = []
-# entrada: 5 números inteiros
-for i in range(5): # for para adicionar 5 números inteiros a lista criada anteriormente
-    num = int(input(f"informe um número[{i+1}]: ")) # entrada de número inteiro para adicionar a lista
-    lista.append(num) # adicionar número inteiro a lista criada anteriormente
 
-# saida: 5 números inteiros e suas posições
+# -- Entrada de dados --
+
+lista = [] # Criando uma lista vazia e adicionar 5 números inteiros a ela.
+
+for i in range(5): # for para adicionar 5 números inteiros a lista criada anteriormente
+    num = int(input(f"Informe um número[{i+1}]: ")) # Entrada de número inteiro para adicionar a lista
+    lista.append(num) # Adicionando número inteiro a lista criada anteriormente
+
+# -- Processamento e Saída de dados --
+
+print() # Print vazio funciona como um enter do teclado
+
 for i in range(5):  # for para imprimir 5 números inteiros e suas posições na lista
     print(f"lista[{lista[i]}] -> {i+1}") # imprimir número inteiro e sua posição na lista
 
-# ou
-print("ou")
-for i in range(5): # for para imprimir 5 números inteiros e suas posições na lista
-    print(f"O número {lista[i]} está na posição [{i}]") # imprimir número inteiro e sua posição na lista
+# Modo 2.0
 
-print("fim do programa")
+# for i in range(5): # for para imprimir 5 números inteiros e suas posições na lista
+#    print(f"O número {lista[i]} está na posição [{i}]") # imprimir número inteiro e sua posição na lista
+
+# Modo 3.0
+
+# for posicao, valor in enumerate(lista): # Enumerate cria um tupla com os valores e suas posições.
+#    print(f'O número {valor} está na posição {posicao}')
+
+print("\nfim do programa") # Informa ao usuário que o programa terminou
