74f4f78d90ad6a64070bdbbea872f0b7b719c56b
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-13 21:56:02 -0300

    Invertendo Listas 🔄📋

--- ex02.py
+++ ex02.py
@@ -4,22 +4,22 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 28/06/2022
+# data: 13/08/2022
 #
-# 1 – 2 – ler uma lista de 5 número reais e imprimir a lista na ordem inversa.
-#
-# criar uma lista vazia e adicionar 5 números inteiros a ela.
-lista = [] # criar uma lista vazia
-# entrada: 5 números reais
+# 2 – ler uma lista de 5 número reais e imprimir a lista na ordem inversa.
+
+# -- Entrada de dados --
+
+lista = [] # Criando uma lista vazia e adicionando 5 números inteiros a ela.
+
 for i in range(5): # for para adicionar 5 números reais a lista criada anteriormente
-    num = float(input(f"informe um número[{i+1}]: ")) # entrada de número real para adicionar a lista
-    lista.append(num) # adicionar número real a lista criada anteriormente
+    num = float(input(f"Informe um número[{i+1}]: ")) # Solicitando valores reais
+    lista.append(num) # Adicionando número real a lista criada anteriormente
 
-# saida:
-print("lista inversa:") # imprimir a lista inversa
-print(f" usando [::-1] = {lista[::-1]}") # imprimir a lista inversa com o comando [::-1]
-lista.reverse() # inverter a lista
-# o método reverse() inverte a ordem da lista mas sem ordena-la
-print(f"usando o método .reverse = {lista}")
+# -- Processamento e saída de dados --
 
-print("fim do programa")
+print(f"\nLista na ordem inversa: {lista[::-1]}") # Imprimindo a lista inversa com o comando [::-1]
+
+# lista.reverse() # Invertendo a lista usando o método .reverse(), ele inverte a ordem da lista mas sem ordena-la
+
+print("\nfim do programa") # Informando ao usuário que o programa chegou ao fim
