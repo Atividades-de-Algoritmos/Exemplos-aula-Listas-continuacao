156d2a3f4113650e73c1c75e9f3c7aadad72a2f5
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-13 21:56:29 -0300

    Calculando Média ➗➕

--- ex03.py
+++ ex03.py
@@ -4,23 +4,27 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 28/06/2022
+# data: 13/07/2022
 #
 # 3 – ler uma lista de 4 notas e em seguida mostra
 # as notas e a média.
-#
-# criar uma lista vazia e adicionar 4 números inteiros a ela.
-listaNotas = [] # criar uma lista vazia
-# entrada: 4 números inteiros
+
+# -- Entrada de dados --
+
+listaNotas = [] # Criando uma lista vazia -> list()
+
 for i in range(4): # for para adicionar 4 números inteiros a lista criada anteriormente
-    nota = int(input(f"informe a nota[{i+1}]: ")) # entrada da nota para adicionar a lista
-    listaNotas.append(nota) # adicionar nota a lista criada anteriormente
+    nota = int(input(f"Informe a nota[{i+1}]: ")) # Solicitando um valor inteiro para atribuir a nota
+    listaNotas.append(nota) # Adicionando nota a lista criada anteriormente
 
-# saida:
-print("notas:") # imprimir as notas
+# -- Processamento e saída de dados --
+
+print("\nnotas: ") # Imprimindo as notas
+
 for i in range(4): # for para imprimir as notas e a média
-    print(f"nota {i+1} = {listaNotas[i]}") # imprimir nota da lista
-media = sum(listaNotas) / len(listaNotas) # calcular a média  da lista
-print(f"média = {media}") # imprimir a média
-print("fim do programa") # imprimir fim do programa
+    print(f"nota {i+1} = {listaNotas[i]}") # Imprimindo as nota da lista
 
+media = sum(listaNotas) / len(listaNotas) # Calculando a média da lista
+print(f"\nmédia = {media}") # Imprimindo a média das notas
+
+print("\nfim do programa") # Informando ao usuário que o programa terminou
